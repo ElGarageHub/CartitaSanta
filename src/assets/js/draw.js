891a6e8b13ec8f5c53845c77a3f6375079393425
javierrizzoa <javierrizzoa@gmail.com>
2018-11-27 03:49:16 +0000

    Add support for touch interfaces

--- src/assets/js/draw.js
+++ src/assets/js/draw.js
@@ -1,6 +1,5 @@
-  var xCoord,yCoord="";
 document.body.onload = function() {
-  console.log('asdf');
+  var xCoord,yCoord="";
   var clic=false;
   var canvas=document.getElementById("can");
   var cntx=canvas.getContext("2d");
@@ -10,34 +9,49 @@ document.body.onload = function() {
   cntx.fillStyle="#fff";
   cntx.fillRect(0,0,canvas.width,canvas.height);
 
-  canvas.onmousedown = function(event) {
-    console.log('1');
+  canvas.onmousedown = canvas.ontouchstart = function(event) {
+    if (event.target == canvas) {
+      event.preventDefault();
+    }
     clic=true;
     cntx.save();
-    xCoord=event.clientX-canvas.getBoundingClientRect().left;
-    yCoord=event.clientY-canvas.getBoundingClientRect().top;
+    if(event.type == 'mousedown') {
+      xCoord=event.clientX-canvas.getBoundingClientRect().left;
+      yCoord=event.clientY-canvas.getBoundingClientRect().top;
+    } else if(event.type == 'touchstart') {
+      xCoord=event.changedTouches[0].clientX-canvas.getBoundingClientRect().left;
+      yCoord=event.changedTouches[0].clientY-canvas.getBoundingClientRect().top;
+    }
   };
 
-  document.onmouseup = function() {
-    console.log('2');
-    clic=false
+  document.onmouseup = document.ontouchend = canvas.ontouchend= function(event) {
+    if (event.target == canvas) {
+      event.preventDefault();
+    }
+    clic=false;
   };
 
-  document.onclick = function() {
-    console.log('3');
-    clic=false
-  };
-
-  canvas.onmousemove = function(event){
-    console.log('4');
+  canvas.onmousemove = canvas.ontouchmove = function(event){
+    if (event.target == canvas) {
+      event.preventDefault();
+    }
     if(clic==true) {
       cntx.beginPath();
-      cntx.moveTo(event.clientX-canvas.getBoundingClientRect().left,event.clientY-canvas.getBoundingClientRect().top);
+      if(event.type == 'mousemove') {
+        cntx.moveTo(event.clientX-canvas.getBoundingClientRect().left,event.clientY-canvas.getBoundingClientRect().top);
+      } else if(event.type == 'touchmove') {
+        cntx.moveTo(event.changedTouches[0].clientX-canvas.getBoundingClientRect().left,event.changedTouches[0].clientY-canvas.getBoundingClientRect().top);
+      }
       cntx.lineTo(xCoord,yCoord);
       cntx.stroke();
       cntx.closePath();
-      xCoord=event.clientX-canvas.getBoundingClientRect().left;
-      yCoord=event.clientY-canvas.getBoundingClientRect().top;
+      if(event.type == 'mousemove') {
+        xCoord=event.clientX-canvas.getBoundingClientRect().left;
+        yCoord=event.clientY-canvas.getBoundingClientRect().top;
+      } else if(event.type == 'touchmove') {
+        xCoord=event.changedTouches[0].clientX-canvas.getBoundingClientRect().left;
+        yCoord=event.changedTouches[0].clientY-canvas.getBoundingClientRect().top;
+      }
     }
   };
-};
+}
